1e1655a54cfafc58ecb42add036169f5300101e2
KanMingKai <tkps50803@gmail.com>
2026-05-19 15:08:43 +0800

    收藏頁面也好了~~

--- server.js
+++ server.js
@@ -126,8 +126,6 @@ async function handleMerge(req, res) {
 
 // ── 主伺服器 ──
 http.createServer((req, res) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
